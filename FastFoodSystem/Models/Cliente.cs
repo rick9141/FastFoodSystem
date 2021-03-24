84e39fb69ac3877682bbd6580f530f6de6a81e58
mgbatista <maiara.27bueno@gmail.com>
2021-03-24 15:19:34 -0300

    alterando models de biblioteca para pastas

--- FastFoodSystem/Models/Cliente.cs
+++ FastFoodSystem/Models/Cliente.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 
-namespace Model
+namespace FastFoodSystem.Models
 {
     public class Cliente
     {
