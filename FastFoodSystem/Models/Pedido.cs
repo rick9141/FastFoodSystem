eef117d22ed12724d97ec016b7850af9149f816f
mgbatista <maiara.27bueno@gmail.com>
2021-03-25 15:38:37 -0300

    Criacao do metodo Realizar Pedido, create e edit

---
+++ FastFoodSystem/Models/Pedido.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FastFoodSystem.Models
+{
+    public class Pedido
+    {
+        [Key]
+        public int IdPedido { get; set; }
+        public virtual Cliente Cliente { get; set; }
+        //public string Cpf { get; set; }
+        public virtual Refeicao Refeicao { get; set; }
+        //public int IdRefeicao { get; set; }
+        //public string Tipo { get; set; }
+        //public string Valor { get; set; }
+        public string FormaPag { get; set; }
+        public virtual StatusPedido StatusPedido { get; set; }
+        //public int IdStatus { get; set; }
+        //public string Descricao { get; set; }
+    }
+}
